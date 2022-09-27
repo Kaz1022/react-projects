88207adb82cc8b528d3110417347254cc1788948
Kazuko Baynton <kazukobaynton@gmail.com>
2022-09-27 10:25:18 -0700

    add random function

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -31,6 +31,16 @@ const Review = () => {
     })
   }
 
+  const randomPerson = () => {
+    // it will be 0 ~ 3.-----. 
+    let randomNumber = Math.floor(Math.random() * people.length);
+    // avoid repetition of same index 
+    if (randomNumber === index){
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  }
+
   return (
     <article className='review'>
       <div className='img-container'>
@@ -50,7 +60,7 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-        <button className="random-btn">
+        <button className="random-btn" onClick={randomPerson}>
           surprise me
         </button>
     </article>
