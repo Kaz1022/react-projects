9281341da2486ab7e8fb48b9ee8ff7f8ea28c46e
Kazuko Baynton <kazukobaynton@gmail.com>
2022-09-28 14:51:41 -0700

    add side bar

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -28,28 +28,48 @@ function App() {
     );
   }
 
+  // need to destructure right after loading is false
+  // since jobs are empty array first
   const { company, dates, duties, title } = jobs[value];
-  return <section className="section">
-    <div className="title">
-      <h2>experience</h2>
-      <div className="underline"></div>
-    </div>
-    <div className="jobs-center">
-      {/*  btn container */}
-      {/* job info */}
-      <article className="job-info">
-        <h3>{title}</h3>
-        <h4>{company}</h4>
-        <p className="job-date">{dates}</p>
-        {duties.map((duty, index)=> {
-          return <div key={index} className="job-desc">
-            <FaAngleDoubleRight className="job-icon"/>
-            <p>{duty}</p>
-          </div>
-        })}
-      </article>
-    </div>
-    </section>;
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/*  btn container */}
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button 
+                key={job.id} 
+                onClick={()=> setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+                >
+                {job.company}
+              </button>
+            );
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
