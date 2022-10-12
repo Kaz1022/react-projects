32e89729a61283bb994e67ebf6dea36cfd184911
Kazuko Baynton <kazukobaynton@gmail.com>
2022-10-12 13:04:08 -0700

    complete color generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -6,16 +6,17 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);      
+      setError(false);
+      setList(colors);
     } catch (error) {
       setError(true);
-      console.log(error)
+      console.log(error);
     }
   };
 
@@ -29,7 +30,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
-            className={`${error? 'error' : null}`}
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
             submit
@@ -37,7 +38,9 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return <SingleColor key={index} color={color} index={index} />;
+        })}
       </section>
     </>
   );
